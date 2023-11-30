2fbb70417d799fad4bee83a167a91675f0e69507
Sai-Saran-26 <128400624+Sai-Saran-26@users.noreply.github.com>
2023-11-30 16:10:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Header";
+import Todo from "./Todo";
+
+function App() {
+  return (
+  <div>
+   <Header />
+   <Todo />
+   </div>
+  );
+}
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header()
+{
+return (
+    <h1 className="head">A Task Management App</h1>
+);
+}
+export default Header;
